f83e3891a40752520345d82fd3cca3de9cc7e221
caveman <46973458+cdrummey216@users.noreply.github.com>
2024-12-21 10:53:48 -0500

    Create logchain.js

---
+++ src/controllers/logchain.js
@@ -0,0 +1,45 @@
+const Entries = require('../models/entries');
+const Logchain = require('../models/logchain');
+const Nodes = require('../models/nodes');
+
+class LogchainController {
+    constructor(url, port) {
+        this.logchain = new Logchain(url, port);
+        this.nodes = new Nodes(url, port);
+        this.entries = new Entries();
+    }
+
+    resolve(req, res) {
+        this.nodes.resolve(res, this.logchain);
+    }
+
+    getNodes(req, res) {
+        res.json(this.nodes.list);
+    }
+
+    postEntry(req, res) {
+        this.entries.add(req, res);
+    }
+
+    getEntries(req, res) {
+        res.json(this.entries.get());
+    }
+
+    mine(req, res) {
+        res.json(this.logchain.mine(this.entries, res));
+    }
+
+    getLogchain(req, res) {
+        res.json(this.logchain.logs);
+    }
+
+    getLogByIndex(req, res) {
+        res.json(this.logchain.getLogByIndex(req.params.idx));
+    }
+
+    getLogLastIndex(req, res) {
+        res.json(this.logchain.getLogLastIndex());
+    }
+}
+
+module.exports = LogchainController;
