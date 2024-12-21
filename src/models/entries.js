065ab205bd8c8cb010fea2cd70f36fd2eab69198
caveman <46973458+cdrummey216@users.noreply.github.com>
2024-12-21 17:24:20 -0500

    Update entries.js

--- src/models/entries.js
+++ src/models/entries.js
@@ -11,7 +11,7 @@ class Entries {
         try {
             let entry = new Entry(req.body.lastGuid, req.body.status, req.body.fortune);
             this.list.push(entry);
-            response = {'success': 1};
+            response = {'success': entry.guid};
 
         } catch(ex) {
             res.status(406);
