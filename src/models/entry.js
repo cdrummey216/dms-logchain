bf9dcbdb93702d13d75d8a40b47a242004c2a48a
caveman <46973458+cdrummey216@users.noreply.github.com>
2024-12-20 19:08:31 -0500

    Update entry.js

--- src/models/entry.js
+++ src/models/entry.js
@@ -1,1 +1,14 @@
-item
+class Entry {
+    constructor(lastGuid, status, fortune) {
+        if (!lastGuid || !status || !fortune)
+            throw new Error('Invalid data');
+
+        this.guid = ;
+        this.lastGuid = lastGuid;
+        this.status = status;
+        this.timestamp = Math.floor(+new Date() / 1000);
+        this.fortune = fortune;
+    }
+}
+
+module.exports = Entry;
