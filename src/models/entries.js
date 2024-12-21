a0ce99b06238f296b5342b1aed26093e0dd2706a
caveman <46973458+cdrummey216@users.noreply.github.com>
2024-12-20 19:27:04 -0500

    Update entries.js

--- src/models/entries.js
+++ src/models/entries.js
@@ -1,1 +1,33 @@
-testing
+const Entry = require('./entry');
+
+class Entries {
+    constructor() {
+        this.list = [];
+    }
+
+    add(req, res) {
+        let response = '';
+
+        try {
+            let entry = new Entry(req.body.lastGuid, req.body.status, req.body.fortune);
+            this.list.push(entry);
+            response = {'success': 1};
+
+        } catch(ex) {
+            res.status(406);
+            response = {'error': ex.message};
+        }
+
+        res.json(response);
+    }
+
+    get() {
+        return this.list;
+    }
+
+    reset() {
+        this.list = [];
+    }
+}
+
+module.exports = Entries;
