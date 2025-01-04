48e4cef4f62f592b73e3303b9bad54feff8c1206
caveman <46973458+cdrummey216@users.noreply.github.com>
2025-01-04 18:27:24 -0500

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -11,12 +11,10 @@ function getCookieValue(name) {
     const regex = new RegExp(`(^| )${name}=([^;]+)`)
     const match = document.cookie.match(regex)
     if (match) {
-      //console.log(match[2]);
       return match[2];
     }
  };
  function setInputValue1() {
- console.log("guidcookie");
   const lastGuidCookie = getCookieValue('lastGuid');
   document.getElementById("currentAccount").innerHTML = lastGuidCookie;
 }
@@ -31,7 +29,6 @@ function setInputValue3() {
 };
 function addEntry(){
   const lastGuidi = document.getElementById("lastGuid").value;
-  console.log(lastGuidi);
   const lastLogi = document.getElementById("lastLog").value;
   const statusi = document.getElementById("status").value;
   const fortunei = document.getElementById("fortune").value;
@@ -42,7 +39,6 @@ function addEntry(){
     status: statusi,
     fortune: fortunei
   };
-  console.log(postEntryUrl);
   (async () => {
     const rawResponse = await fetch(postEntryUrl, {
       method: 'POST',
@@ -55,18 +51,14 @@ function addEntry(){
     const contenti = await rawResponse.json();
     const contentii = JSON.stringify(contenti);
     const guid = contentii.replace(/"/g, '');
-    console.log(guid);
     const fileNamei = Math.floor(+new Date() / 1000);
     const fileNameii = fileNamei + ".txt";
-    setCookie('lastGuid', guid, 7);
-    //setCookie('key', fileNameii, 7);
-    setCookie('timestamp', fileNamei, 7);
+    setCookie('lastGuid', guid, 14);
+    setCookie('timestamp', fileNamei, 14);
     createAndDownloadFile(fileNameii, guid);
     updateCache(lastGuidi, fileNamei, guid);
-    console.log(guid);
   })();
 };
-
 function createAndDownloadFile(fileName, content) {
   const blob = new Blob([content], { type: "text/plain" });
   const url = URL.createObjectURL(blob);
@@ -79,38 +71,31 @@ function createAndDownloadFile(fileName, content) {
 };
 function updateCache(oldguid, timestamp, newguid) {
   var updateCacheUrl = window.location.origin + "/lode/" + oldguid + "/" + timestamp + "/" + newguid;
-  console.log(updateCacheUrl);
   fetch(updateCacheUrl)
     .then(response => response.json())
     .then(data => {
-      console.log("Cache Updated for  guid: " + newguid);
+      console.log("Cache Updated for uid: " + newguid);
     })
     .catch(error => console.error('Error fetching data:', error));
 };
 function findLogIdx(guid) {
   var lastGuid = guid;
-  var lastLog = document.getElementById("lastLog").value;
   var findLogIdxUrl = window.location.origin + "/logchain/log/" + lastGuid;
-  console.log(findLogIdxUrl);
   fetch(findLogIdxUrl)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        console.log("lastLog");
-      setCookie('lastLog', JSON.stringify(data), 7);                      
+      setCookie('lastLog', JSON.stringify(data), 14);                      
       document.getElementById("lastLog").value = data;
     })
     .catch(error => console.error('Error fetching data:', error));
 };
 function mineEntries() {
   var mineEntriesUrl = window.location.origin + "/mine";
-  console.log(mineEntriesUrl);
   fetch(mineEntriesUrl)
     .then(response => response.json())
     .then(data => {
       const table = document.getElementById('thisTable');
       const tbody = table.getElementsByTagName('tbody')[0];
-      console.log(data.entries);
       data.entries.forEach(item => {
         const row = tbody.insertRow();
         Object.values(item).forEach(value => {
@@ -126,19 +111,18 @@ function lodeUIDs() {
     const lastGuidCookie = getCookieValue('lastGuid');
     const timestampCookie = getCookieValue('timestamp');
       var updateCacheUrl = window.location.origin + "/lode/10000000-1000-4000-8000-100000000000/" + timestampCookie + "/" + lastGuidCookie;
-      console.log(updateCacheUrl);
       fetch(updateCacheUrl)
         .then(response => response.json())
         .then(data => {
-          console.log("Cache Updated for  guid: " + data);
+          console.log("Cache Updated for uid: " + data);
         })
         .catch(error => console.error('Error fetching data:', error));
 };
 function findHistory() {
   var lastGuid = document.getElementById("lastGuid").value;
-  var lastLog = document.getElementById("lastLog").value;
+
   var findHistoryUrl = window.location.origin + "/logchain/history/" + lastGuid;
-  console.log(findHistoryUrl);
+
   fetch(findHistoryUrl)
     .then(response => response.json())
     .then(data => {
@@ -157,10 +141,9 @@ function findHistory() {
 }
 function findEntry() {
   var lastGuid = document.getElementById("lastGuid").value;
-  var lastLog = document.getElementById("lastLog").value;
+
   var findEntryUrl = window.location.origin + "/logchain/entry/" + lastGuid;
-  console.log(findEntryUrl);
-  fetch(findEntryUrl)
+  consfetch(findEntryUrl)
     .then(response => response.json())
     .then(data => {
       const table = document.getElementById('thisTable');
